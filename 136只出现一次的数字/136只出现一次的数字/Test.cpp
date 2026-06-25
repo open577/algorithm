6805c7539188eb54c96f4f0c04270f66a68a0f70
王彦逸 <2171498913@qq.com>
2026-06-25 14:08:09 +0800

    位运算

---
+++ 136只出现一次的数字/136只出现一次的数字/Test.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+//给你一个 非空 整数数组 nums ，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+//
+//你必须设计并实现线性时间复杂度的算法来解决此问题，且该算法只使用常量额外空间。
+//
+//
+//
+//示例 1 ：
+//
+//输入：nums = [2, 2, 1]
+//
+//输出：1
+//
+//示例 2 ：
+//
+//输入：nums = [4, 1, 2, 1, 2]
+//
+//输出：4
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ret = 0;
+        for (auto x : nums) ret ^= x;
+        return ret;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
