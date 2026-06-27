061d827097a5227c1382172c39468c5b72e9c9da
王彦逸 <2171498913@qq.com>
2026-06-28 00:12:11 +0800

    位运算

---
+++ 371两整数之和/371两整数之和/Test.cpp
@@ -0,0 +1,36 @@
+﻿#include<iostream>
+using namespace std;
+
+//给你两个整数 a 和 b ，不使用 运算符 + 和 - ​​​​​​​，计算并返回两整数之和。
+//
+//
+//
+//示例 1：
+//
+//输入：a = 1, b = 2
+//输出：3
+//示例 2：
+//
+//输入：a = 2, b = 3
+//输出：5
+
+class Solution {
+public:
+    int getSum(int a, int b) {
+        while (b)
+        {
+            int x = a ^ b;
+            int y = (a & b) << 1;
+
+            a = x, b = y;
+        }
+        return a;
+    }
+};
+
+int main()
+{
+
+
+    return 0;
+}
